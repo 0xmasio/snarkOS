78ffad55a6c0b0834754ddf36a868e6214bf3345
0xmasio <144013507+0xmasio@users.noreply.github.com>
2023-11-26 12:50:13 +0330

    Update lib.rs
    Added a line
    
    Signed-off-by: 0xmasio <144013507+0xmasio@users.noreply.github.com>

--- account/src/lib.rs
+++ account/src/lib.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![forbid(unsafe_code)]
+#![forbid(unsafe_code)] 
 
 use snarkvm::{
     console::{network::prelude::*, types::Field},
